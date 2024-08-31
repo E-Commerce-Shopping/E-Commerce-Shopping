ecbaa39194b30502991072a4ccf0815db0939877
DELL <kalagoudapatil508@gmail.com>
2024-08-31 17:56:23 +0530

    My Commit on Current Reposistory

---
+++ src/test/java/com/shopping/E_Commerce/Shopping/ECommerceShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.E_Commerce.Shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
